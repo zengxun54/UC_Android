3ed1a864439d46dc316fa0d60fa1dfb8f9cff8bd
yl1698 <linmx@yealink.com>
2018-09-27 14:00:29 +0800

    上传登录模块

---
+++ Test_case/Logins/Login.py
@@ -0,0 +1,130 @@
+# -*- encoding:utf-8 -*-
+__author__ = 'zhonghaitao'
+import unittest
+from appium import webdriver
+import time
+import config_factory
+import Const
+
+
+class Login(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.config_factory=config_factory.ConfigFactory()
+        print('setup')
+        # 启动app
+        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', self.config_factory.desired_cups)
+        # 启动app时，需要一定时间进入引导页，所以必须设置等待时间，不然下面会一直报错定位不到元素
+        time.sleep(3)
+        # 检查是否有系统权限弹窗
+        self.config_factory.always_allow(self.driver,3)
+
+    @classmethod
+    def tearDownClass(self):
+        print('tearDown')
+        # pass
+
+    # @unittest.skip("demonstrating skipping")
+    def test_first_install_welcome(self):
+        '''
+        首次安装进入的是引导页
+
+        '''
+        #等待欢迎页面出现
+        self.driver.wait_activity('com.yealink.uc.android.login.WelcomeActivity',2,2)
+        #向右滑动3张引导图
+        self.config_factory.swipeToRight(self.driver,200,3)
+        # 点击立即体验按钮
+        self.config_factory.find_element_id(self.driver,Const.EXPETIENCE).click()
+
+    def test_login_checkui(self):
+        '''
+            正常用例：校验ui
+            检查点：页面元素，值
+        '''
+        self.driver.wait_activity("com.yealink.uc.android.login.LoginActivity",2,2)
+        #用户名
+        username=self.config_factory.find_element_id(self.driver,Const.USERNAME).text
+        #密码
+        password=self.config_factory.find_element_id(self.driver,Const.PASSWORD).text
+        #服务器
+        server=self.config_factory.find_element_id(self.driver,Const.SERVER).text
+        #记住密码
+        ckb_remember_psw=self.config_factory.find_element_id(self.driver,Const.REMENBERPASSWORD).get_attribute("checked")
+
+        #忘记密码
+        forgetpassword=self.config_factory.find_element_id(self.driver,Const.FORGETPASSWORD).text
+        #意见反馈
+        feedback=self.config_factory.find_element_id(self.driver,Const.FEEDBACK).text
+        #校验默认值
+        self.assertEqual(username,"",msg="用户名输入框默认值不对")
+        self.assertEqual(password,"",msg="密码默认值不对")
+        self.assertEqual(server,"ume.yealink.com",msg="服务器默认值不对")
+        self.assertTrue(ckb_remember_psw, msg="默认选中状态不对")
+        self.assertEqual(forgetpassword,"忘记密码",msg="忘记密码文案错误")
+        self.assertEqual(feedback,"意见反馈",msg="意见反馈文案错误")
+        self.assertTrue(self.config_factory.find_element_id(self.driver,Const.FORGETPASSWORD).get_attribute("Clickable"),"忘记密码不能点击")
+        self.assertTrue(self.config_factory.find_element_id(self.driver,Const.FEEDBACK).get_attribute("Clickable"),"意见反馈不能点击")
+
+    def test_login_errorhost(self):
+        '''
+            异常用例：输入错误的服务器地址
+            检查点：错误提示是否正确
+        '''
+        #输入数据进行登录操作
+        self.config_factory.login(self.driver,self.config_factory.login_errorhost["username"],self.config_factory.login_errorhost["password"],self.config_factory.login_errorhost["server"])
+        #校验提示
+        self.config_factory.asserttips(self.driver,self.config_factory.login_errorhost["title"],self.config_factory.login_errorhost["message"])
+
+    def test_login_errorusername(self):
+        '''
+            异常用例：输入错误的用户名
+            检查点：错误提示是否正确
+        '''
+        # 输入数据进行登录操作
+        self.config_factory.login(self.driver, self.config_factory.login_errorusername["username"],self.config_factory.login_errorusername["password"],self.config_factory.login_errorusername["server"])
+        # 校验提示
+        self.config_factory.asserttips(self.driver, self.config_factory.login_errorusername["title"],self.config_factory.login_errorusername["message"])
+
+    def test_login_notexistusername(self):
+        '''
+            异常用例：输入不存在的用户名
+            检查点：错误提示是否正确
+        '''
+        # 输入数据进行登录操作
+        self.config_factory.login(self.driver, self.config_factory.login_notexistusername["username"],self.config_factory.login_notexistusername["password"],self.config_factory.login_notexistusername["server"])
+        # 校验提示
+        self.config_factory.asserttips(self.driver, self.config_factory.login_notexistusername["title"],self.config_factory.login_notexistusername["message"])
+
+    def test_login_errorpassword(self):
+        '''
+            异常用例：输入错误的密码
+            检查点：错误提示是否正确
+        '''
+        # 输入数据进行登录操作
+        self.config_factory.login(self.driver, self.config_factory.login_errorpassword["username"],self.config_factory.login_errorpassword["password"],self.config_factory.login_errorpassword["server"])
+        # 校验提示
+        self.config_factory.asserttips(self.driver, self.config_factory.login_errorpassword["title"],self.config_factory.login_errorpassword["message"])
+
+    def test_login_nullhost(self):
+        '''
+            异常用例：服务器地址为空
+            检查点：错误提示是否正确
+        '''
+        # 输入数据进行登录操作
+        self.config_factory.login(self.driver, self.config_factory.login_nullhost["username"],self.config_factory.login_nullhost["password"],self.config_factory.login_nullhost["server"])
+        # 校验提示
+        self.config_factory.asserttips(self.driver, self.config_factory.login_nullhost["title"],self.config_factory.login_nullhost["message"])
+
+    def test_login_zq(self):
+        '''
+            正常用例：输入的数据都正确
+            检查点：登录是否成功
+        '''
+        # 输入数据进行登录操作
+        self.config_factory.login(self.driver, self.config_factory.login_zq["username"],self.config_factory.login_zq["password"],self.config_factory.login_zq["server"])
+
+        #日程权限
+        self.config_factory.always_allow(self.driver,3)
+        #校验当前activity是否为mainactivity
+        self.assertEqual(self.driver.current_activity,'com.yealink.uc.android.MainActivity',msg="未登录成功")
---
+++ Test_case/Logins/config_factory.py
@@ -0,0 +1,156 @@
+# coding=utf-8
+__author__ = 'zhonghaitao'
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import Const
+APP_URL="com.yealink.uc.android:id/"
+
+class ConfigFactory(object):
+    # ------------------------------------------------------------------------
+    #配置
+    # desired_cups={
+    #     "platformName": "Android",
+    #     "platformVersion": "6.0",
+    #     "deviceName": "8fdda31",
+    #     "appWaitActivity": "com.yealink.uc.android.login.LoginActivity",
+    #     "noReset": "True",
+    #     "app": r"F://Yealink_UC_Android_1.1.3.839-pre-debug.apk",
+    # }
+    # desired_cups={
+    #     "platformName": "Android",
+    #     "platformVersion": "6.0",
+    #     "deviceName": "69DDU16513003941",
+    #     "appWaitActivity": "com.yealink.uc.android.login.LoginActivity",
+    #     "noReset": "True",
+    #     "app": r"F://Yealink_UC_Android_1.1.3.839-pre-debug.apk",
+    # }
+    # 50e35a0
+    # desired_cups={#红米5red plus+lmx PC
+    #     "platformName": "Android",
+    #     "platformVersion": "7.1.2",
+    #     "deviceName": "8452f49e0804",
+    #     "appWaitActivity": "com.yealink.uc.android.login.LoginActivity",
+    #     "noReset": "True",
+    #     "app": r"F:\UME android apk\Yealink_UC_Android_1.1.16.1024-local-release.apk",
+    # }
+    desired_cups={#小米4+lmx PC
+        "platformName": "Android",
+        "platformVersion": "7.0.0",
+        "deviceName": "M960BDQD22BWB",
+        "appWaitActivity": "com.yealink.uc.android.login.LoginActivity",
+        "noReset": "True",
+        "app": r"F:\UME android apk\Yealink_UC_Android_1.1.17.1027-local-release.apk",
+    }
+    # ------------------------------------------------------------------------
+    #用例数据
+    login_errorhost={
+        "username":  "cs247@uctest.yealink.com",
+        "password" : "6pREAdem",
+        "server" : "sjsjsj",
+        "title":"登录失败",
+        "message":"连接服务器失败，请检查网络或者服务器地址！"
+    }
+    login_errorusername={
+        "username": "d",
+        "password": "Zhht@080",
+        "server": "uctest.yealink.com",
+        "title": "登录失败",
+        "message": "用户名或密码错误"
+    }
+    login_notexistusername = {
+        "username": "ddsdsdsds",
+        "password": "Zhht@080",
+        "server": "uctest.yealink.com",
+        "title": "登录失败",
+        "message": "没有找到对应账号"
+    }
+    login_errorpassword = {
+        "username":  "cs247@uctest.yealink.com",
+        "password" : "6pREAdem@hh",
+        "server": "uctest.yealink.com",
+        "title": "登录失败",
+        "message": "用户名或密码错误，请重新输入"
+    }
+    login_nullhost = {
+        "username":  "cs247@uctest.yealink.com",
+        "password" : "6pREAdem",
+        "server": "",
+        "title": "登录失败",
+        "message": "服务器地址不能为空"
+    }
+    login_zq = {
+        "username": "cs247@uctest.yealink.com",
+        "password": "6pREAdem",
+        "server": "uctest.yealink.com",
+    }
+
+
+    # ------------------------------------------------------------------------
+    #封装方法
+    #首次安装后进入应用的权限弹窗处理
+    def always_allow(self,driver,number):
+        # 判断是否有权限弹窗,number为判断弹窗次数
+        for i in range(number):
+            loc = ("xpath", "//*[@text='允许']")
+            try:
+                e = WebDriverWait(driver, 1, 1).until(EC.presence_of_element_located(loc))
+                e.click()
+            except:
+                pass
+
+    #向右滑动
+    def swipeToRight( self,driver, during, num) :
+        x = driver.get_window_size()['width']
+        y = driver.get_window_size()['height']
+        for i in range(num):
+            driver.swipe(x * 6 / 7 , y / 2 , x / 7 , y / 2 , during)
+            time.sleep(4)
+
+    #向左滑动
+    def swipeToLeft( self,driver, during, num) :
+        print(driver.get_window_size())
+        x = driver.get_window_size()['width']
+        y = driver.get_window_size()['height']
+        for i in range(num):
+            driver.swipe(x / 7 , y / 2 , x * 5 / 7 , y / 2 , during)
+            time.sleep(3)
+    #针对密码输入框进行清空操作
+    def editClear(self,driver, el):
+        el.click()
+        driver.keyevent(123)
+        for i in range(0, 8):
+             driver.keyevent(67)
+
+    def find_element_id(self,driver,id):
+        element=driver.find_element_by_id(APP_URL+id)
+        return element
+
+    def login(self,driver,username,password,server):
+        # 赋值
+        driver.find_element_by_id(APP_URL + Const.USERNAME).send_keys(username)
+        self.editClear(driver, driver.find_element_by_id(APP_URL + Const.PASSWORD))
+        driver.find_element_by_id(APP_URL + Const.PASSWORD).send_keys(password)
+        driver.find_element_by_id(APP_URL + Const.SERVER).send_keys(server)
+        driver.hide_keyboard()  # 隐藏输入法
+
+        # 登录操作
+        driver.find_element_by_id(APP_URL + Const.LOGIN).click()
+        time.sleep(3)
+
+    def  asserttips(self,driver,title,message):
+        # 获取提示值
+        title1 = driver.find_element_by_id(APP_URL + Const.FAILTITLE).text
+        message1 = driver.find_element_by_id(APP_URL + Const.FAILMESSAGE).text
+        # print(message)
+        # 校验提示
+        if title ==title1:
+            pass
+        else:
+            raise ("提示标题不正确")
+        if message == message1:
+            pass
+        else:
+            raise ("提示名称不正确")
+        # 关闭提示
+        driver.find_element_by_id(APP_URL+ Const.FAILBUTTON).click()
